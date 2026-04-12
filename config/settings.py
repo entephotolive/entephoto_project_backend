4de61def7ae389004a1c34505b018876b7d7a5e1
sahalcheruth <sahalxcheruth@gmail.com>
2026-04-13 02:44:07 +0530

    new commit

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
+import dj_database_url
 load_dotenv()
 
 # ─────────────────────────────────────────────────────────────────────────────
@@ -68,18 +68,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # ─────────────────────────────────────────────────────────────────────────────
 # Database — Railway PostgreSQL
 # ─────────────────────────────────────────────────────────────────────────────
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-        'CONN_MAX_AGE': 600,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#         'CONN_MAX_AGE': 600,
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 # ─────────────────────────────────────────────────────────────────────────────
 # Password validation
 # ─────────────────────────────────────────────────────────────────────────────
